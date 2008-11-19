640f28cc28fa2d40846eb1b571dc31466418a066
n.salhi.74 <n.salhi.74@df7d7cd4-b5b2-11dd-a067-9b1d96bc3259>
2008-11-19 10:25:39 +0000

    modifying the pdf of CPU IO bursts..

--- ProcessGenerator.java
+++ ProcessGenerator.java
@@ -28,6 +28,9 @@ class ProcessGenerator
 		int iob_min = 0;
 		FileOutputStream fos; 
 		DataOutputStream dos;
+		double segma_cpu =  Math.sqrt( 1/( 2*Math.PI*75*75 ) );// 0.005;//0.005321; 1/ (  2*Math.PI*75*75 )
+		double segma_io = Math.sqrt( 1/( 2*Math.PI*25*25 ) );
+		int mean = 0;//numOfProcesses/2;
 		
 		try {
 
@@ -35,18 +38,19 @@ class ProcessGenerator
 		    fos = new FileOutputStream(file);
 		    dos=new DataOutputStream(fos);
 	
-			int i =0;
+			int i =0 , j = - (numOfProcesses/2);
 			while( i < numOfProcesses ) {
 				pID = i;
 				new PrintStream(dos).print(i);
 				new PrintStream(dos).print(" ");
-				CPU_Bursts = (int) (Math.random() * (cb_max - cb_min + 1) ) + cb_min;
+
+				CPU_Bursts =  (int)  ( ( 1/( Math.sqrt( 2*Math.PI*(Math.pow(segma_cpu,2) ) ) ) ) * Math.exp( -(Math.pow( (j - mean ),2) ) / 2*Math.pow(segma_cpu,2) ) ); // (int) (Math.random() * (cb_max - cb_min + 1) ) + cb_min;
 				new PrintStream(dos).print(CPU_Bursts);
 				new PrintStream(dos).print(" ");
 				CPU_BurstTime = (int) (Math.random() * (ct_max - ct_min + 1) ) + ct_min;
 				new PrintStream(dos).print(CPU_BurstTime);
 				new PrintStream(dos).print(" ");
-				IO_Bursts = (int) (Math.random() * (iob_max - iob_min + 1) ) + iob_min;
+				IO_Bursts = (int)  ( ( 1/( Math.sqrt( 2*Math.PI*(Math.pow(segma_io,2) ) ) ) ) * Math.exp( -(Math.pow( (j - mean ),2) ) / 2*Math.pow(segma_io,2) ) ); // (int) (Math.random() * (cb_max - cb_min + 1) ) + cb_min;//(int) (Math.random() * (iob_max - iob_min + 1) ) + iob_min;
 				new PrintStream(dos).print(IO_Bursts);
 				new PrintStream(dos).print(" ");
 				IO_BurstTime = (int) (Math.random() * (iot_max - iot_min + 1) ) + iot_min;
@@ -55,8 +59,9 @@ class ProcessGenerator
 				new PrintStream(dos).println("");
 				
 				i++;
+				j++;
 			}
-			
+		//	( 1/( Math.sqrt( 2*PI*(pow(segma,2) ) ) ) ) * exp( -(pow( (i - mean),2) ) / 2*pow(segma,2) ) ;
 			    // Close  output stream
 			    dos.close();
 	
